6ca4e520fe1b333cd1363d25c56e6efbd855283d
Sharad Shrestha <sshrest3@ucsc.edu>
2018-09-21 01:57:35 -0700

    Create SQLQueryAlter.sql

---
+++ SQLQueryAlter.sql
@@ -0,0 +1,23 @@
+ALTER TABLE dbo.runner
+ALTER COLUMN Age float null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN City nvarchar null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN DivPl nvarchar null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN Name nvarchar null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN Sex nvarchar null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN SexPl nvarchar null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN State nvarchar null;
+
+ALTER TABLE dbo.runner
+ALTER COLUMN Time nvarchar null;
