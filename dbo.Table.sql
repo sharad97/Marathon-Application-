42744586685b4148638caa12586fbc32a89ced4d
Sharad Shrestha <sshrest3@ucsc.edu>
2018-10-03 04:35:44 -0700

    marathon coordination and sql table
    marathon track coordination and user sql data table added

---
+++ dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UserID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [EmailID] VARCHAR(254) NOT NULL, 
+    [DateOfBirth] DATETIME NULL, 
+    [Password] NVARCHAR(MAX) NOT NULL, 
+    [IsEmailVerified] bit NOT NULL, 
+    [ActivationCode] UNIQUEIDENTIFIER NOT NULL
+)
